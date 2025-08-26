58bc1d62b2bb00f369dd5eeb9e44b448cdb47d5a
Samih <sakmohamed@gmail.com>
2025-08-26 13:11:38 +0400

    feat: adding hot reloading (WIP)

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #define LOAD_CSV_H_IMPLEMENTATION
+
 #include "ft_linear_regression.h"
+#include <dlfcn.h>
+
+typedef int (*f_ptr)(int, int);
 
 void run_lr(point_t *points)
 {
@@ -9,7 +13,33 @@ void run_lr(point_t *points)
     for (size_t i = 0; i < n_points; ++i)
     {
         fprintf(log, "[%f-%f]\n", points[i].milage, points[i].price);
+        fprintf(stdout, "[%f-%f]\n", points[i].milage, points[i].price);
     }
+}
+
+void recompile()
+{
+    static const char *compile = "cc -c -fPIC add.c && cc -shared add.o -o libdyn.so";
+    system(compile);
+}
+
+void *get_symbol(const char *shared_path, const char *sym)
+{
+    static void *handle;
+    if (handle)
+    {
+        dlclose(handle);
+        handle = NULL;
+    }
+    recompile();
+    // handle = dlmopen(LM_ID_NEWLM, shared_path, RTLD_NOW);
+    handle = dlopen(shared_path, RTLD_NOW);
+    if (!handle)
+    {
+        fprintf(stderr, "Error: %s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+    return dlsym(handle, sym);
 }
 
 int main()
@@ -20,10 +50,16 @@ int main()
     InitWindow(WINDOW_W, WINDOW_H, TITLE);
     SetTargetFPS(20);
 
+    Camera2D camera = {0};
+    camera.target = (Vector2){0,0};
+    camera.zoom = 1;
+
     point_t *points;
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        BeginMode2D(camera);
+
         ClearBackground(DARKGRAY);
         DrawPlotArea();
         DrawMenu(&action);
@@ -40,6 +76,8 @@ int main()
         default:
             break;
         }
+
+        EndMode2D();
         EndDrawing();
     }
     CloseWindow();
