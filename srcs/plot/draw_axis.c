60908efb7aea7002562c3c2b5a67d6cb8c160905
Samih <sakmohamed@gmail.com>
2025-09-06 23:26:20 +0400

    fix: change draw axis implementation

--- srcs/plot/draw_axis.c
+++ srcs/plot/draw_axis.c
@@ -2,7 +2,7 @@
 
 void draw_axis(Camera2D camera)
 {
-    float lt = 3.0f * (1/camera.zoom);
+    float lt = 3.0f * (1 / camera.zoom);
     Color lc = BLUE;
 
     Vector2 x_axis_start = {
@@ -27,25 +27,3 @@ void draw_axis(Camera2D camera)
     DrawLineEx(y_axis_start, y_axis_end, lt, lc);
     DrawLineEx(x_axis_start, x_axis_end, lt, lc);
 }
-// void draw_axis(data_t t)
-// {
-//     float lt = 2.0f;
-//     Color lc = BLUE;
-
-//     Vector2 x_axis_start = data_to_window(t, (point_t){
-//                                                  .x = t.min_x - t.max_x,
-//                                                  .y = 0.0f});
-//     Vector2 x_axis_end = data_to_window(t, (point_t){
-//                                                .x = t.max_x + t.max_x,
-//                                                .y = 0.0f});
-
-//     Vector2 y_axis_start = data_to_window(t, (point_t){
-//                                                  .x = 0,
-//                                                  .y = t.min_y - t.max_y});
-//     Vector2 y_axis_end = data_to_window(t, (point_t){
-//                                                .x = 0,
-//                                                .y = t.max_y * 2});
-
-//     DrawLineEx(x_axis_start, x_axis_end, lt, lc);
-//     DrawLineEx(y_axis_start, y_axis_end, lt, lc);
-// }
